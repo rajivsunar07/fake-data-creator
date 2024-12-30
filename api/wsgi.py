657509252543636d1609abdd3e7729f587c159b6
rajivsunar <rajivsunar07@gmail.com>
2024-12-30 22:36:42 +0545

    changed for vercel

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,4 +1,4 @@
-from app import app  # Import the app instance from app.py
+from api.index import app  # Import the app instance from app.py
 
 if __name__ == "__main__":
     app.run(debug=True)  # Optional for testing
